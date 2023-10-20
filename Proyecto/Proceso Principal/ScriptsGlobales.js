23a1cdd82a98614f0cf63a2adfca7aa6fe06d78f
Cristhian Ortiz <144409689+TheCrisMars@users.noreply.github.com>
2023-10-19 23:00:27 -0500

    Update ScriptsGlobales.js

--- Proyecto/Proceso Principal/ScriptsGlobales.js
+++ Proyecto/Proceso Principal/ScriptsGlobales.js
@@ -1,59 +1,73 @@
-// Función para subir un proyecto
-function subirProyecto() {
-  // Obtener los valores de los campos del formulario
-  const nombreEstudiante = document.getElementById('nombreEstudiante').value.trim();
-  const tituloProyecto = document.getElementById('tituloProyecto').value.trim();
-  const asesor = document.getElementById('asesor').value;
-  const archivoProyecto = document.getElementById('archivoProyecto').files[0];
-  const confirmacionAutor = document.getElementById('confirmacionAutor').checked;
-
-  // Validar que todos los campos estén completos y se haya confirmado que es el autor propio
-  if (nombreEstudiante === '' || tituloProyecto === '' || asesor === '0' || archivoProyecto === '' || !confirmacionAutor) {
-      alert('Por favor, complete todos los campos y confirme que es el autor propio.');
-      return; // Salir de la función si no se cumple la validación
-  }
-
-  // Elementos HTML de la lista de trabajos
-  const trabajosList = document.querySelector('#trabajosList');
-  const listItem = document.createElement('div');
-  listItem.className = 'list-group-item';
-
-  // Obtener el nombre del asesor seleccionado
-  const asesorText = document.getElementById('asesor').options[document.getElementById('asesor').selectedIndex].text;
-
-  // Verificar si el archivo es un PDF
-  if (archivoProyecto.type === 'application/pdf') {
-      listItem.innerHTML = `
-          <h6 class="mb-2">Título: ${tituloProyecto}</h6>
-          <p class="mb-2">Estudiante: ${nombreEstudiante}</p>
-          <p>Asesor: ${asesorText}</p>
-          <button class="btn btn-danger btn-sm" onclick="eliminarTrabajo(this)">Eliminar</button>
-      `;
-      trabajosList.appendChild(listItem);
-
-      // Limpiar los campos después de agregar el trabajo
-      document.getElementById('nombreEstudiante').value = '';
-      document.getElementById('tituloProyecto').value = '';
-      document.getElementById('asesor').value = '0';
-      document.getElementById('archivoProyecto').value = '';
-      document.getElementById('confirmacionAutor').checked = false;
-  } else {
-      alert('El archivo debe ser un PDF.');
-  }
+// Función para redirigir a la página de "upload.html"
+function redirectToUpload() {
+  window.location.href = "upload.html";
 }
 
-// Función para eliminar un trabajo de la lista
-function eliminarTrabajo(button) {
-  const listItem = button.parentElement;
-  listItem.remove();
-}
 
 // Función para validar campos de entrada
 function validarCampo(input) {
   const valor = input.value.trim();
   if (valor === '') {
-      input.classList.add('is-invalid');
+      input.classList.add('invalid');
   } else {
-      input.classList.remove('is-invalid');
+      input.classList.remove('invalid');
+  }
+}
+
+// Función para validar una dirección de correo electrónico
+function validarEmail() {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const emailInput = document.getElementById("registroEmailInput");
+  const resultadoEmail = document.getElementById("resultadoEmail");
+  
+  if (!emailRegex.test(emailInput.value)) {
+      resultadoEmail.textContent = "La dirección de correo electrónico no es válida.";
+      return false;
+  }
+  
+  resultadoEmail.textContent = "";
+  return true;
+}
+
+// Función para validar un número de teléfono
+function validarTelefono() {
+  const phoneRegex = /^\d{10}$/; // Se asume que el número debe contener exactamente 10 dígitos
+  const telefonoInput = document.getElementById("telefono");
+  const resultadoTelefono = document.getElementById("resultadoTelefono");
+  
+  if (!phoneRegex.test(telefonoInput.value.replace(/\D/g, ''))) {
+      resultadoTelefono.textContent = "El número de teléfono no es válido. Debe contener 10 dígitos.";
+      return false;
+  }
+  
+  return true;
+}
+
+// Función para validar contraseñas
+function validarContrasena() {
+  const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{10,}$/;
+  const contrasenaInput = document.getElementById("contrasena");
+  const resultadoContrasena = document.getElementById("resultadoContrasena");
+
+  if (!passwordRegex.test(contrasenaInput.value)) {
+      resultadoContrasena.textContent = "La contraseña no es válida. Debe contener al menos un carácter en mayúscula, al menos un dígito y tener al menos 10 caracteres.";
+      return false;
+  }
+  resultadoContrasena.textContent = "";
+  return true;
+}
+
+// Función para verificar que las contraseñas coincidan
+function verificarContrasenas() {
+  const contrasenaInput = document.getElementById("contrasena").value;
+  const confirmarContrasenaInput = document.getElementById("confirmarContrasena").value;
+  const mensajeContrasenas = document.getElementById("mensajeContrasenas");
+
+  if (contrasenaInput !== confirmarContrasenaInput) {
+      mensajeContrasenas.textContent = "Las contraseñas no coinciden.";
+      return false;
+  } else {
+      mensajeContrasenas.textContent = "";
+      return true;
   }
 }
